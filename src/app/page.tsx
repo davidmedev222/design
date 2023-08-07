b7a9b4619a89fd130e090636ce58a788388608de
davidmedev222 <davidmedev@gmail.com>
2023-08-07 13:15:12 -0300

    chore: first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-center'>
+      <h1 className='text-center text-5xl'>Hello Word</h1>
+    </main>
+  )
+}
+
+export default HomePage
